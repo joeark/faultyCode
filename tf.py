4bb3484655d5d3cc34957cfe5847f3dae90006a7
jayesh thakare <thakarej91@gmail.com>
2025-04-23 22:57:32 +0530

    Fix for issue #15: Transformer module not working - Update tf.py

---
+++ tf.py
@@ -0,0 +1,12 @@
+import torch
+
+x=torch.randn(2,4)
+y=torch.randn(2)
+
+linear_layer_1= torch.nn.Linear(4,16)
+relu_layer = torch.nn.ReLU()
+linear_layer_2=torch.nn.Linear(16,1)
+
+sequential= torch.nn.Sequential(linear_layer_1, relu_layer, linear_layer_2)
+
+y=sequential(x)
